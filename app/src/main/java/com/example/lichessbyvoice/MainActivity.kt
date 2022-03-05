7997d214c86e00fda698cdc9319bb790d8c044b6
Ralf Stephan <ralf@ark.in-berlin.de>
2022-03-05 10:31:28 +0100

    display a game by opening the game endpoint

--- app/src/main/java/com/example/lichessbyvoice/MainActivity.kt
+++ app/src/main/java/com/example/lichessbyvoice/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.lichessbyvoice
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val theButton: Button = findViewById(R.id.button)
+        theButton.setOnClickListener { gameView() }
+    }
+    private fun gameView() {
+        val gameUrl: Uri = Uri.parse("https://lichess.org/0YquyYuP")
+        val intent = Intent(Intent.ACTION_VIEW, gameUrl)
+        val v: View = findViewById(R.id.main)
+        v.context.startActivity(intent)
     }
 }
