8c94b4ad373361ab89479cf44c20dcd036efe2c1
Ralf Stephan <ralf@ark.in-berlin.de>
2022-04-08 16:47:08 +0200

    fix gradle settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "7.1.3" apply false
-//    id("com.android.library") version "7.1.2" apply false
     kotlin("jvm") version "1.6.20" // or kotlin("multiplatform") or any other kotlin plugin
 }
 
