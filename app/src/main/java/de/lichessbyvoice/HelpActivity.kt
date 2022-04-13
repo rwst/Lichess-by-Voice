c7533ed8bb006d2c32ae44d532310aebed915f57
Ralf Stephan <ralf@ark.in-berlin.de>
2022-04-13 19:09:34 +0200

    help activity, finished

--- app/src/main/java/de/lichessbyvoice/HelpActivity.kt
+++ app/src/main/java/de/lichessbyvoice/HelpActivity.kt
@@ -1,16 +1,20 @@
 package de.lichessbyvoice
 
+import android.os.Build
 import android.os.Bundle
 import android.text.Html
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class HelpActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val txtView = findViewById(R.id.help_textView)
-//        val str = find(R.string.help_textview)
-//        txtView.setText(Html.fromHtml(
         setContentView(R.layout.help_activity)
+        val txtView: TextView = findViewById(R.id.help_textView)
+        val str = resources.getString(R.string.help_textview)
+        txtView.text = Html.fromHtml(str, Html.FROM_HTML_MODE_COMPACT)
     }
 }
 
