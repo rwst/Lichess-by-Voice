0d2ac7c6d25be763cf2846b9406d79739df16bd3
Ralf Stephan <ralf@ark.in-berlin.de>
2022-07-05 19:39:31 +0200

    privacy web page

--- web/upload.sh
+++ web/upload.sh
@@ -1,1 +1,1 @@
-scp -i /home/ralf/.ssh/kudu /home/ralf/AndroidStudioProjects/LichessbyVoice/*.html ark@shell.in-berlin.de:~/websites/lichessbyvoice.de/
+scp -i /home/ralf/.ssh/kudu /home/ralf/AndroidStudioProjects/LichessbyVoice/web/*.html ark@shell.in-berlin.de:~/websites/lichessbyvoice.de/
